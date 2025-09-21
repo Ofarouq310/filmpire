e2ffb579ea02e5c92980c10df7d18488f5074755
Ofarouq310 <ofarouq310@gmail.com>
2025-09-21 21:43:31 +0300

    Updated qureies to fetch movies based on genre or category

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,16 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const tmdbApiKey = import.meta.env.VITE_TMDB_API_KEY;
-const page = 1;
 
 
 export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
     baseQuery: fetchBaseQuery ({ baseUrl: 'https://api.themoviedb.org/3/' }),
     endpoints: (builder) => ({
-        // Get Movies by type
         getMovies: builder.query({
-            query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`
+            query: ({genreOrCategoryName, page = 1}) => {
+                if (genreOrCategoryName && typeof genreOrCategoryName === "string") {
+                    return `movie/${genreOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+                }
+
+                if (genreOrCategoryName && typeof genreOrCategoryName === "number") {
+                    return `discover/movie?with_genres=${genreOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+                }
+
+                return `movie/now_playing?page=${page}&api_key=${tmdbApiKey}`;
+            }
         }),
     }),
 })
