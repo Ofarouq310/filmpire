42a3678b9ae7959e054ce68e04f20382225f78bb
Ofarouq310 <ofarouq310@gmail.com>
2025-09-22 23:48:22 +0300

    Add searchQueries for search funtionality

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -2,22 +2,22 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const tmdbApiKey = import.meta.env.VITE_TMDB_API_KEY;
 
-
 export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
     baseQuery: fetchBaseQuery ({ baseUrl: 'https://api.themoviedb.org/3/' }),
     endpoints: (builder) => ({
         getMovies: builder.query({
-            query: ({genreOrCategoryName, page = 1}) => {
+            query: ({genreOrCategoryName, page = 1, searchQuery = ''}) => {
+                if (searchQuery){
+                    return `search/movie?query=${encodeURIComponent(searchQuery)}&page=${page}&api_key=${tmdbApiKey}`;
+                }
                 if (genreOrCategoryName && typeof genreOrCategoryName === "string") {
                     return `movie/${genreOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
                 }
-
                 if (genreOrCategoryName && typeof genreOrCategoryName === "number") {
                     return `discover/movie?with_genres=${genreOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
                 }
-
-                return `movie/now_playing?page=${page}&api_key=${tmdbApiKey}`;
+                    return `movie/now_playing?page=${page}&api_key=${tmdbApiKey}`;
             }
         }),
     }),
