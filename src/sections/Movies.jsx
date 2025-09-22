0426bd8f39f1c1c69e15f0b08b13adc6f3966deb
Ofarouq310 <ofarouq310@gmail.com>
2025-09-22 23:50:31 +0300

    Remove extra setMovie state and refactor code for better performance

--- src/sections/Movies.jsx
+++ src/sections/Movies.jsx
@@ -1,59 +1,73 @@
-import { useGetMoviesQuery } from '../services/TMDB'
-import { useState, useEffect } from 'react'
+import { useGetMoviesQuery } from '../services/TMDB';
 import MovieCard from '../components/MovieCard';
 import Spinner from '../components/Spinner';
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 import Pagination from '@mui/material/Pagination';
 import { selectPage } from '../features/currentGenreOrCategory';
 
-
 export default function Movies() {
+  const { genreOrCategoryName, page, searchQuery } = useSelector(
+    (state) => state.currentGenreOrCategory
+  );
 
-    const {genreOrCategoryName, page} = useSelector((state) => state.currentGenreOrCategory);
-    const {data, error, isFetching } = useGetMoviesQuery({genreOrCategoryName, page});
-    const [movies, setMovies] = useState([]);
+  const { data, error, isFetching } = useGetMoviesQuery({
+    genreOrCategoryName,
+    page,
+    searchQuery,
+  });
 
-    useEffect(() => {
-      if (data?.results) {
-        setMovies(data.results);
-      }
-    }, [data]);
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-
-
-    if (isFetching) return <div className='w-full h-screen text-center flex items-center justify-center p-10'><Spinner /></div>;
-    if (error) return <p className="w-full flex items-center justify-center p-10 text-red-500">Something went wrong!</p>;
-
+  if (isFetching)
     return (
-        <section className='flex flex-col items-center justify-center mb-10'>
-          <div className='p-8 all-movies'>
-            <ul>
-              {movies.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </ul>
-          </div>
-            <Pagination
-              count={data?.total_pages} 
-              size='large' 
-              color='secondary'  
-              showFirstButton 
-              showLastButton
-              page={page}
-               onChange={(event, value) => {
-                dispatch(selectPage(value));
-              }}
-              sx={{
-                "& .MuiPaginationItem-root": {
-                  color: "white",         
-                },
-                "& .Mui-selected": {
-                  color: "secondary",       
-                  backgroundColor: "black",
-                },
-              }}
-              />
-        </section>
-  )
+      <div className="w-full h-screen text-center flex items-center justify-center p-10">
+        <Spinner />
+      </div>
+    );
+
+  if (error)
+    return (
+      <p className="w-full flex items-center justify-center p-10 text-red-500">
+        Something went wrong!
+      </p>
+    );
+
+  if (!data?.results?.length)
+    return (
+      <p className="w-full flex items-center justify-center p-10 text-gray-400">
+        No movies found.
+      </p>
+    );
+
+  return (
+    <section className="flex flex-col items-center justify-center mb-10">
+      <div className="p-8 all-movies">
+        <ul>
+          {data.results.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+        </ul>
+      </div>
+      <Pagination
+        count={Math.min(data.total_pages, 500)} // TMDB only allows 500 pages
+        size="large"
+        color="secondary"
+        showFirstButton
+        showLastButton
+        page={page}
+        onChange={(event, value) => {
+          dispatch(selectPage(value));
+        }}
+        sx={{
+          "& .MuiPaginationItem-root": {
+            color: "white",
+          },
+          "& .Mui-selected": {
+            color: "secondary",
+            backgroundColor: "black",
+          },
+        }}
+      />
+    </section>
+  );
 }
