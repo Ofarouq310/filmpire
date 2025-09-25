f35ae51f096232c292afbd21d70e11975cde9d83
Ofarouq310 <ofarouq310@gmail.com>
2025-09-25 22:54:49 +0300

    Added Link to movie information page inside movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,9 @@
 import noPoster from "../assets/imgs/no-movie.png"
 import Rating from "@mui/material/Rating"
 import { styled } from "@mui/styles";
+import { Link } from "react-router-dom";
 
-export default function MovieCard({movie:{poster_path, title, vote_average}}) {
+export default function MovieCard({movie:{id: movie_id, poster_path, title, vote_average}}) {
 
     const StyledRating = styled(Rating)({
     '& .MuiRating-iconFilled': {
@@ -13,16 +14,16 @@ export default function MovieCard({movie:{poster_path, title, vote_average}}) {
   },
 });
 
-
   return (
     <div className="movie-card hover:scale-102 transform transition-all duration-300 ease-in-out cursor-pointer">
-
-      <img src={poster_path
-        ? `https://image.tmdb.org/t/p/w500/${poster_path}`
-        : noPoster
+      <Link to={`movie/${movie_id}`} >
+        <img src={poster_path
+          ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+          : noPoster
         } 
         alt={title} 
-      />
+        />
+      </Link>
 
       <div className='mt-0.5 p-4 text-center flex flex-col items-center'>
         <h3>{title}</h3>
