9ac296b0f2ea2013dce18297fe9592f6a64157f3
Ofarouq310 <ofarouq310@gmail.com>
2025-09-25 23:25:26 +0300

    Refactor MovieInformation component layout for improved presentation

--- src/sections/MovieInformation.jsx
+++ src/sections/MovieInformation.jsx
@@ -30,7 +30,30 @@ export default function MovieInformation() {
       );
   
   return (
-    <div className="bg-white text-black h-dvh">MovieInformation : {movie.overview} </div>
+    <section className="bg-white text-black h-screen">
+      <div className="flex space-around w-full py-7 px-12 gap-5">
+        <div className='bg-amber-100 flex-1'>
+          <div className="rounded-full">
+            <img 
+              src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+              alt={`${movie.poster} poster`}
+              className="size-full rounded-2xl shadow-black shadow-2xl"
+            />
+          </div>
+        </div>
+        <div className='flex-2 pt-2'>
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h2 className= "text-center">{movie.title}</h2>
+
+            <h3>{movie.tagline}</h3>
+
+            <div className=""></div>
+
+            <div className=""></div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
         
