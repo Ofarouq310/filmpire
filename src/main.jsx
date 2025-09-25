12ee57dbb59c2b61a1c12e49193edf4b081cdc58
Ofarouq310 <ofarouq310@gmail.com>
2025-09-25 22:54:24 +0300

    Created router with muiltiple routes for movie and movie information pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Movies from './sections/Movies.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
+import MovieInformation from './sections/MovieInformation.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Movies />,
+      },
+      {
+        path: 'movie/:id',
+        element: <MovieInformation />,
+      },
+    ],
   },
-])
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
