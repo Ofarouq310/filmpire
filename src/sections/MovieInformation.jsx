5fe83499aafdb6fe8babb2710a8c719c53778166
Ofarouq310 <ofarouq310@gmail.com>
2025-09-28 23:29:36 +0300

    Add recommendations and cast sections to MovieInformation component

--- src/sections/MovieInformation.jsx
+++ src/sections/MovieInformation.jsx
@@ -1,8 +1,21 @@
 import { useParams } from "react-router-dom"
-import { useMovieDetailsQuery } from '../services/TMDB';
+import { useMovieDetailsQuery, useGetRecommendationsQuery} from '../services/TMDB';
 import Spinner from '../components/Spinner';
 import Rating from "@mui/material/Rating"
 import { styled } from "@mui/styles";
+import Cast from "../components/Cast";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MovieIcon from '@mui/icons-material/Movie';
+import WebsiteIcon from '@mui/icons-material/Language';
+import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Filter1Icon from '@mui/icons-material/Filter1';
+import { Link } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
+
 
 export default function MovieInformation() {
 
@@ -16,16 +29,26 @@ export default function MovieInformation() {
   });
 
   const params = useParams();
-  const { data:movie, error, isFetching } = useMovieDetailsQuery( params.id );   
+  const {
+  data: movie,
+  error: movieError,
+  isFetching: movieLoading,
+} = useMovieDetailsQuery(params.id);
+
+const {
+  data: recommendations,
+  error: recError,
+  isFetching: recLoading,
+} = useGetRecommendationsQuery(params.id);
   
-  if (isFetching)
+  if (movieLoading)
     return (
         <div className="w-full h-screen text-center flex items-center justify-center p-10">
           <Spinner />
         </div>
       );
       
-      if (error)
+      if (movieError)
         return (
       <p className="w-full flex items-center justify-center p-10 text-red-500">
           Something went wrong!
@@ -41,10 +64,18 @@ export default function MovieInformation() {
 
       const date = new Date(`${movie.release_date}`).toDateString().split(' ').slice(1).join(' ');
 
-      return (
-      <section className="text-white h-full flex items-center justify-center bg-gray-800">
+      const officialTrailer =
+        movie.videos?.results?.find(
+          (video) =>
+            video.type === "Teaser" &&
+            video.name.toLowerCase().includes("official teaser") ||
+            video.name.toLowerCase().includes("official trailer")
+        ) ||
+        movie.videos?.results?.find((video) => video.type === "Teaser");
 
-      <div className="size-full flex flex-col rounded-lg md:flex-row md:max-w-lg lg:max-w-5xl py-10 px-5">
+      return (
+      <section className="text-white h-full flex flex-col items-center justify-center bg-gray-800">
+      <div className="size-full flex flex-col rounded-lg md:flex-row md:max-w-lg lg:max-w-5xl py-10 sm:px-5">
         <div className='flex-1 flex justify-center'>
           <div className="rounded-full">
             <img 
@@ -55,7 +86,7 @@ export default function MovieInformation() {
           </div>
         </div>
 
-        <div className='flex-2 px-5'>
+        <div className='flex-2  px-3 smpx-5 flex flex-col gap-3'>
           <div className="flex flex-col justify-between items-center px-4 max-md:py-4 leading-normal text-center">
             <h2 className= "text-center">{movie.title}</h2>
 
@@ -64,7 +95,7 @@ export default function MovieInformation() {
             <div className="w-full flex justify-around items-center mb-5">
               <div className="flex flex-wrap justify-center gap-2">
                <StyledRating readOnly defaultValue={movie.vote_average/2} precision={0.1} />
-               <span>{movie.vote_average} /10</span>
+               <span>{movie.vote_average.toFixed(1)} /10</span>
               </div>
 
               <div className="flex flex-wrap justify-center">
@@ -79,10 +110,49 @@ export default function MovieInformation() {
               </div>
           </div>
 
-          <div className="">
+          <div className="overview">
             <h3>Overview</h3>
             <p>{movie.overview}</p>
           </div>
+
+          <div className="cast">
+            <h3>Top Cast</h3>
+            <div className="flex max-w-full flex-wrap gap-4">
+              {movie?.credits?.cast.slice(0,6).map((actor)=> <Cast key={actor.id} actor={actor} />)}
+            </div>
+          </div>
+          
+          <div className="movie-information-btns flex justify-center flex-wrap gap-2">
+            <div>
+                <Stack direction="row" spacing={0.2}>
+                  <Button href={movie.homepage || null} target="_blank" rel="noopener noreferrer" size="small" variant={movie.homepage ? "contained" : "disabled"} endIcon={<WebsiteIcon />}>Website</Button>
+                  <Button href={`https://www.imdb.com/title/${movie.imdb_id}/`} target="_blank" rel="noopener noreferrer" size="small" variant="contained" endIcon={<LocalMoviesIcon />}>IMDB</Button>
+                  {movie.videos?.results?.length > 0 && (
+                  <Button href="{`https://www.youtube.com/watch?v=${officialTrailer.key}` || null" target="_blank" rel="noopener noreferrer" size="small" variant={officialTrailer ? "contained" : "disabled"} endIcon={<MovieIcon />}>Trailer</Button>
+                  )}
+                  </Stack>
+            </div>
+            
+            <div>
+                <Stack direction="row" spacing={0.2}>
+                  <Button  size="small" variant="contained" endIcon={<FavoriteIcon />}>Favorite</Button>
+                  <Button  size="small" variant="contained" endIcon={<Filter1Icon />}>Watchlist</Button>
+                  <Button component={Link} to="/" size="small" variant="contained" endIcon={<ArrowBackIosIcon />}>Back</Button>
+                </Stack>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+
+      <div className="recommendation flex flex-col items-center justify-center">
+        <h2>You might also like</h2>
+        <div className="grid grid-cols-2 sm:grid-cols-4 mx-2">
+          {recommendations ?
+          recommendations.results.map((movie)=> <MovieCard key={movie.id} movie={movie} />)
+          : <h2>No movies to recommend</h2>
+          }
         </div>
       </div>
       
