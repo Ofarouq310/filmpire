43b8f00df43839b600f0e471e46afb34bdaf8a37
Ofarouq310 <ofarouq310@gmail.com>
2025-10-01 22:35:56 +0300

    Added Link to actor's profile page

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Cast({actor}) {
   return (
     <div className="flex flex-col gap-1">
-      <img src= {`https://image.tmdb.org/t/p/w200/${actor.profile_path}`} alt={actor.name} className="w-20 h-[6.5em]" />
+      <Link to={`/actor/${actor.id}`}>
+        <img src= {`https://image.tmdb.org/t/p/w200/${actor.profile_path}`} alt={actor.name} className="w-20 h-[6.5em]" />
+      </Link>
       <h4 className="text-xs max-w-19">{actor.name}</h4>
       <h5 className="text-xs max-w-19 text-gray-400">{actor.character}</h5>
     </div>
