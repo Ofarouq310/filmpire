5ed39b964cc0bbbd87683265a776007e27874316
Ofarouq310 <ofarouq310@gmail.com>
2025-10-08 21:24:07 +0300

    update placeholder text dynamically based on screen size

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,26 +1,44 @@
-import SearchIcon from '@mui/icons-material/Search';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { searchMovie } from '../features/currentGenreOrCategory';
-import { useNavigate } from 'react-router-dom';
+import SearchIcon from "@mui/icons-material/Search";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { searchMovie } from "../features/currentGenreOrCategory";
+import { useNavigate } from "react-router-dom";
 
 export default function Search() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const currentGenreOrCategory = useSelector(
     (state) => state.currentGenreOrCategory.genreOrCategoryName
   );
 
+  const [placeholder, setPlaceholder] = useState(
+    "Search through thousands of movies"
+  );
+
   useEffect(() => {
-    setQuery('');
+    const handleResize = () => {
+      if (window.matchMedia("(max-width: 640px)").matches) {
+        setPlaceholder("Search movies...");
+      } else {
+        setPlaceholder("Search through thousands of movies");
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    setQuery("");
   }, [currentGenreOrCategory]);
 
   const handleSearch = () => {
     if (query.trim()) {
       dispatch(searchMovie(query.trim()));
-      navigate('/'); // redirect to your main movies page
+      navigate(`/?search=${encodeURIComponent(query)}&page=1`);
     }
   };
 
@@ -33,14 +51,14 @@ export default function Search() {
       <input
         name="search-input"
         type="text"
-        placeholder="Search through thousands of movies"
+        placeholder= {placeholder}
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === 'Enter') handleSearch();
+          if (e.key === "Enter") handleSearch();
         }}
         className="ml-2 bg-transparent text-base placeholder:text-sm placeholder:text-center focus:outline-none placeholder-gray-300 dark:placeholder-gray-700 text-gray-900 dark:text-white w-30 sm:w-65"
-        />
+      />
     </div>
   );
 }
