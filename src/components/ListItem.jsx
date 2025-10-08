d57f22a191838dea348927fedeffaa6d523c852c
Ofarouq310 <ofarouq310@gmail.com>
2025-10-08 21:20:23 +0300

    Add page param in website url while exploring different genres or category

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-export default function ListItem({category, onClick, genre}) {
-    const data = category || genre;
+export default function ListItem({ category, onClick, genre }) {
+  const data = category || genre;
   return (
-    <Link to={`/${data.title}`}>
-      <li className='categories__list-items flex gap-2' onClick={onClick}>{data.logo} <span>{data.title}</span></li>
+    <Link to={`/${encodeURIComponent(data.title)}?page=1`}>
+      <li className="categories__list-items flex gap-2" onClick={onClick}>
+        {data.logo} <span>{data.title}</span>
+      </li>
     </Link>
-  )
+  );
 }
